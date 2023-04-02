978cfe4d9e39e10bcd601a0b99575d5a38e3c617
azzamaddouri <maddouriazza@gmail.com>
2023-04-02 16:24:04 +0200

    I finished implementing user and role models

--- src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
+++ src/main/java/com/spring/jwt/JwtWithSpringSecurityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JwtWithSpringSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JwtWithSpringSecurityApplication.class, args);
 	}
