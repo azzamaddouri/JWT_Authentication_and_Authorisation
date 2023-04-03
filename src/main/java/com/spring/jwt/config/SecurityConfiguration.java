770c48b6b37c6315ee82784250b68e17ad0fed07
azzamaddouri <maddouriazza@gmail.com>
2023-04-03 19:42:00 +0200

    I finished implementing all the configuration

---
+++ src/main/java/com/spring/jwt/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.spring.jwt.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration @EnableWebSecurity @RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+    http.csrf().disable().authorizeHttpRequests().requestMatchers("") /* I want to whitelist this list and authorize all the requests within this list but any other request should be authenticated */
+            .permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)/* Create a session for each request */; // Disable csrf verification
+    // Now we will implement the real configuration , don't need the JWT token in these cases : Create an account
+
+
+    return http.build();
+
+}
+}
